2624a372bd9ce3b18131184b226b31919ba2bb7e
carrot0322 <carrot100322@gmail.com>
2024-05-24 00:14:44 +0900

    init

---
+++ src/main/java/me/carrot0322/commandhook/CommandHook.java
@@ -0,0 +1,16 @@
+package me.carrot0322.commandhook;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommandHook extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
